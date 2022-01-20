d072da5df389eda1dba1fd8e1270dd85d59637b0
shisheloff <45948320+shisheloff@users.noreply.github.com>
2022-01-20 23:17:52 +0300

    entrypoint

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+import psycopg2
+from config import host, user, password, db_name
+
+try:
+    connection = psycopg2.connect(host=host, user=user, password=password, database=db_name)
+
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT version();")
+        print(f"Server version: {cursor.fetchone()}")
+
+except Exception as _ex:
+    print("[INFO] Error while working with postgresql database", _ex)
+finally:
+    if connection:
+        connection.close()
+        print("[INFO] postgresql connection closed")
